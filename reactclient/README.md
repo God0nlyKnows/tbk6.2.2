f9d246258250466d2fceed91625a01354f5c0cb9
God0nlyKnows <37994631+God0nlyKnows@users.noreply.github.com>
2022-11-22 23:47:41 +0100

    add react

---
+++ reactclient/README.md
@@ -0,0 +1,2 @@
+##Requirements
+App needs env WEB_API_BASE_URL
---
+++ reactclient/src/setupProxy.js
@@ -0,0 +1,14 @@
+﻿const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const context = [
+    "/weatherforecast",
+];
+
+module.exports = function (app) {
+    const appProxy = createProxyMiddleware(context, {
+        target: process.env.WEB_API_BASE_URL,
+        secure: false
+    });
+
+    app.use(appProxy);
+};
